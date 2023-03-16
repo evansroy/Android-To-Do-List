6c74cebd3bda701d2f23ee9aa6dba4c96f7e38a8
evansroy <evansroysir@gmail.com>
2023-03-16 20:48:33 +0300

    Add comment and explanation to the code in SplashFragment class

--- app/src/main/java/com/evansroy/todolist/fragments/SplashFragment.kt
+++ app/src/main/java/com/evansroy/todolist/fragments/SplashFragment.kt
@@ -1,60 +1,59 @@
 package com.evansroy.todolist.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Handler
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import com.evansroy.todolist.R
+import com.google.firebase.auth.FirebaseAuth
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [SplashFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
+// Define the SplashFragment class
 class SplashFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    // Declare some private properties for the class
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var navController: NavController
 
+    // Inflate the layout for the fragment
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_splash, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment SplashFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            SplashFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    // Initialize the view and define a handler to execute some code after a delay
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // Call the init method to initialize some properties
+        init(view)
+
+        // Determine if the user is logged in or not
+        val isLogin: Boolean = mAuth.currentUser != null
+
+        // Define a handler to execute some code after a delay
+        val handler = Handler(Looper.myLooper()!!)
+        handler.postDelayed({
+
+            // If the user is logged in, navigate to the home screen
+            if (isLogin)
+                navController.navigate(R.id.action_splashFragment_to_homeFragment)
+            // If the user is not logged in, navigate to the login screen
+            else
+                navController.navigate(R.id.action_splashFragment_to_signInFragment)
+
+        }, 2000) // Delay the execution of the code by 2 seconds
+    }
+
+    // Initialize some properties for the class
+    private fun init(view: View) {
+        mAuth = FirebaseAuth.getInstance()
+        navController = Navigation.findNavController(view)
     }
 }
