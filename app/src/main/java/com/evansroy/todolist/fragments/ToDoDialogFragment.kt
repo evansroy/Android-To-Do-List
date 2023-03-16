9bb6e5f88228ed3eba2f5c15eb6adaeca24e4588
evansroy <evansroysir@gmail.com>
2023-03-16 21:35:02 +0300

    Defines a DialogFragment that allows the user to create a new to-do task

--- app/src/main/java/com/evansroy/todolist/fragments/ToDoDialogFragment.kt
+++ app/src/main/java/com/evansroy/todolist/fragments/ToDoDialogFragment.kt
@@ -1,60 +1,76 @@
 package com.evansroy.todolist.fragments
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.evansroy.todolist.R
+import androidx.fragment.app.DialogFragment
+import com.evansroy.todolist.databinding.FragmentToDoDialogBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+import com.google.android.material.textfield.TextInputEditText
 
-/**
- * A simple [Fragment] subclass.
- * Use the [ToDoDialogFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
-class ToDoDialogFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+// The class extends the DialogFragment class
+class ToDoDialogFragment : DialogFragment() {
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+    // Declare necessary variables
+    private lateinit var binding: FragmentToDoDialogBinding
+    private var listener : OnDialogNextBtnClickListener? = null
+    private var toDoData: ToDoData? = null
+
+    // A function to set the listener for the Next button
+    fun setListener(listener: OnDialogNextBtnClickListener) {
+        this.listener = listener
     }
 
+    // A companion object used to create new instances of this fragment
+    companion object {
+        const val TAG = "DialogFragment"
+
+        // A function to create a new instance of the fragment with given arguments
+        @JvmStatic
+        fun newInstance(taskId: String, task: String) =
+            ToDoDialogFragment().apply {
+                arguments = Bundle().apply {
+                    putString("taskId", taskId)
+                    putString("task", task)
+                }
+            }
+    }
+
+    // Inflate the view and create the binding object
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_to_do_dialog, container, false)
+        binding = FragmentToDoDialogBinding.inflate(inflater , container,false)
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ToDoDialogFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ToDoDialogFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
+    // Initialize the view and set the event listeners
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        // If there are any arguments passed in, create a new instance of the to-do item with the arguments
+        if (arguments != null){
+            toDoData = ToDoData(arguments?.getString("taskId").toString() ,arguments?.getString("task").toString())
+            binding.todoEt.setText(toDoData?.task)
+        }
+
+        // Set the listener for the Close button
+        binding.todoClose.setOnClickListener {
+            dismiss()
+        }
+
+        // Set the listener for the Next button
+        binding.todoNextBtn.setOnClickListener {
+            val todoTask = binding.todoEt.text.toString()
+
+            // If the to-do item is not empty, save it or update it accordingly
+            if (todoTask.isNotEmpty()){
+                if (toDoData == null){
+                    listener?.saveTask(todoTask , binding.todoEt)
+                }else{
+                    toDoData!!.task = todoTask
+                    listener?.updateTask(toDoData!!, binding.todoEt)
                 }
             }
-    }
-}
